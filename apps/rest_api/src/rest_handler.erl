f8e977d7025069bd1280213a30cf2c6671e4914f
Maksim Krzhemenevskiy <maksimk@meforo.com>
2017-01-25 15:48:52 +0700

    basic auth in simplest form

--- apps/rest_api/src/rest_handler.erl
+++ apps/rest_api/src/rest_handler.erl
@@ -18,6 +18,11 @@
 -define(AMOUNT_TOKEN, <<"amount">>).
 -define(NAME_TOKEN, <<"name">>).
 
+-define(USERNAME, <<"admin">>).
+-define(PASSWORD, <<"password">>).
+
+-define(AUTH_HEADER, <<"authorization">>).
+
 %% API
 -export([
   init/3,
@@ -29,6 +34,39 @@ init({tcp, http}, Req, []) ->
   {ok, Req, #state{}}.
 
 handle(Req0, State = #state{}) ->
+  lager:info("trying to handle"),
+  {AuthHeader, Req1} = cowboy_req:header(?AUTH_HEADER, Req0),
+  Creds = creds_from_header(AuthHeader),
+  lager:info("got creds: ~p", [Creds]),
+  RetReq = case Creds of
+             {?USERNAME, ?PASSWORD} ->
+               lager:info("running query"),
+               run_query(Req1);
+             _ ->
+               lager:info("running auth"),
+               run_auth(Req1)
+           end,
+  {ok, RetReq, State}.
+
+run_auth(Req0) ->
+  Req1 = cowboy_req:set_resp_header(<<"Www-Authenticate">>, <<"Basic realm=\"Secure Area\"">>, Req0),
+  Req2 = cowboy_req:set_resp_body(<<"Ha-ha!">>, Req1),
+  {ok, Req3} = cowboy_req:reply(401, Req2),
+  Req3.
+
+creds_from_header(undefined) -> undefined;
+creds_from_header(Header) ->
+  lager:info("Header: ~p", [Header]),
+  Creds = case binary:split(Header, <<" ">>) of
+            [<<"Basic">>, Base64Creds] -> Base64Creds;
+            _ -> undefined
+          end,
+  case cowboy_http:authorization(Creds, <<"basic">>) of
+    {<<"basic">>, Result} -> Result;
+    _ -> undefined
+  end.
+
+run_query(Req0) ->
   {ok, RetReq} = case cowboy_req:method(Req0) of
                    {<<"GET">>, GetReq0} ->
                      {Action, GetReq1} = cowboy_req:qs_val(<<"action">>, GetReq0, <<"best_players">>),
@@ -37,7 +75,7 @@ handle(Req0, State = #state{}) ->
                    {_, ElseReq0} ->
                      cowboy_req:reply(404, ?CONTENT_TYPE_TEXT, <<"Not found. \n\nP.S. Nginx guilty">>, ElseReq0)
                  end,
-  {ok, RetReq, State}.
+  RetReq.
 
 terminate(_Reason, _Req, _State) ->
   ok.
